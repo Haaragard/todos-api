83928101133f6b424f98081242df5c621a1ec7b8
Haaragard <haaragard@gmail.com>
2022-04-02 17:46:29 -0300

    fixed todos listing response

--- app/Http/Controllers/ListTodosController.php
+++ app/Http/Controllers/ListTodosController.php
@@ -15,6 +15,6 @@ class ListTodosController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return Todo::all();
+        return response(Todo::all());
     }
 }
