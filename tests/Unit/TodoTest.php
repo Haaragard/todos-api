5baecc74a888aaee4df36870fd663f55f4142f2f
Haaragard <haaragard@gmail.com>
2022-04-02 23:37:41 -0300

    fixed todos unit register test

--- tests/Unit/TodoTest.php
+++ tests/Unit/TodoTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Unit;
 
 use App\Models\Todo;
+use App\Models\TodoList;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -13,7 +15,9 @@ class TodoTest extends TestCase
     /** @test */
     public function can_register_a_todo()
     {
-        $todo = Todo::factory()->create();
+        $user = User::factory()->create();
+        $todoList = $user->todoLists()->create(TodoList::factory()->raw());
+        $todo = $todoList->todos()->create(Todo::factory()->raw());
 
         $this->assertDatabaseHas(Todo::class, $todo->toArray());
     }
